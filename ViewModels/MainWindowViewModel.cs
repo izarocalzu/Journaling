1a3cb3d6064fea1173ad9b33f4548fef6bc4a7e0
dam52 <dam52@gmail.com>
2025-09-30 19:44:31 +0200

    cambios en el model

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace Journaling.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Journaling.Models;
+
+namespace Journaling.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Titulo { get; } = "SONGS JOURNAL";
     public string Saludo { get; } = "¡Añade tus canciones favoritas!";
+    
+    [ObservableProperty] private Cancion cancion = new();
 }
